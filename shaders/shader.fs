a4d15d2400aaed3cc084e9f88b3d84d8d60c241f
Lorenzo PILOTTO <lpilotto@e3r11p18.42.fr>
2017-09-13 17:20:28 +0200

    one color per face

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -5,6 +5,7 @@ out vec4 frag_colour;
 in float dist;
 
 void main() {
-	float color = clamp(1.0 / dist, 0.0, 1.0);
+	//float color = clamp(1.0 / dist, 0.0, 1.0);
+	float color = 0.15 + ((gl_PrimitiveID % 9 + 1) / 9.0) / 0.85;
 	frag_colour = vec4(color, color, color, 1.0);
 }
